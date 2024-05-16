5a0bbc70ee29b32a8521751ebd4245e66b72a4e8
singh <singhkailash2001@gmail.com>
2024-05-16 15:00:58 +0530

    text display using button

--- react/hookusestate/usestate/src/User.jsx
+++ react/hookusestate/usestate/src/User.jsx
@@ -3,16 +3,30 @@ import React, { useState } from "react"
 function User(){
 
    let [content,setCount]=useState( ' ')
+  
+    function gettext(e){ 
    
-   function gettext(e){
        setCount(e.target.value)
+
+    
+
    }
+   let [text,settext]=useState(' ')
+   function click(){
+     settext(content )
+   }
+   
+  
+      
+   
+   
 
 
     return(
         <>
-        <h1>{content}</h1>
+        <h1>{text}</h1>
             <input type="text"  onChange={gettext} />
+            <button onClick={click}>submit</button>
         </>
     )
 }
